c25a8792668331bbced10f17b8ff62a81dec3364
dendyindriyo <dendyindriyo@gmail.com>
2023-06-01 08:55:12 +0700

    Adding test suite TSuite_02

---
+++ Test Suites/TSuite_02_Auth.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSuite_02_Auth</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d349fca1-2ddd-46bf-832c-dc8511b17e7e</testSuiteGuid>
+   <testCaseLink>
+      <guid>e4f1f391-4205-4bc6-b3a0-93170aa8c93d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Auth/Auth - CreateToken</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
