43a29e810e80a245f12a81170888fc8c2c7d7845
dendyindriyo <dendyindriyo@gmail.com>
2023-06-01 08:52:25 +0700

    Adding test suite TSuite_01

---
+++ Test Suites/TSuite_01_Check Server.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSuite_01_Check Server</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3e18e14e-9a41-4c93-a2f7-a4f53ea261ef</testSuiteGuid>
+   <testCaseLink>
+      <guid>97697d4a-26ff-4791-8060-e55417d93917</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Ping/Ping - HealthCheck</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
